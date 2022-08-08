10be738b59821665221bca6544f0638053e72b1a
jonas-mika <accounts@jonassenghaas.de>
2022-08-08 17:19:58 +0200

    redis either locally or remote depending on env

--- config.py
+++ config.py
@@ -10,15 +10,17 @@ from dotenv import load_dotenv
 
 # load env
 load_dotenv()
+DB_URI = os.environ['DB_URI']
+REDIS_URL = os.environ['REDISCLOUD_URL'] if 'REDISCLOUD_URL' in os.environ else os.environ['REDIS_LOCAL']
 
 class Config:
   SECRET_KEY = os.environ["SECRET_KEY"]
 
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SQLALCHEMY_ECHO = True
-  SQLALCHEMY_DATABASE_URI = os.environ["DB_URI"]
+  SQLALCHEMY_DATABASE_URI = DB_URI
 
   SESSION_TYPE = 'redis'
   SESSION_PERMANENT = False
   SESSION_USE_SIGNER = True
-  SESSION_REDIS = redis.from_url(os.environ['REDISCLOUD_URL'])
+  SESSION_REDIS = redis.from_url(REDIS_URL)
