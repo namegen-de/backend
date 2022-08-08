ae2515a89e5c4043405717728ccb65bb3e213fcb
jonas-mika <accounts@jonassenghaas.de>
2022-08-08 15:59:46 +0200

    changed redis url to heroku addon

--- config.py
+++ config.py
@@ -21,4 +21,4 @@ class Config:
   SESSION_TYPE = 'redis'
   SESSION_PERMANENT = False
   SESSION_USE_SIGNER = True
-  SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+  SESSION_REDIS = redis.from_url(os.environ['REDISCLOUD_URL'])
